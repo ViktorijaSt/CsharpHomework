d5576281bb5b5f4f750f4cf8cf18a446a436080a
Viktorija <viktorija_stefanovska@yahoo.com>
2019-03-08 12:38:44 +0100

    homework02 code

---
+++ homework2/App1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is behind door number: 1, 2, 3 - please select your number..");
+
+
+            string caseSwitch;
+            caseSwitch = Console.ReadLine();
+            int intSwitch = int.Parse(caseSwitch);
+            switch (intSwitch)
+            {
+                case 1:
+                    Console.WriteLine("You got a new car.");
+                    break;
+                case 2:
+                    Console.WriteLine("You got a new plane.");
+                    break;
+                case 3:
+                    Console.WriteLine("You got a new bike.");
+                    break;
+                default:
+                    Console.WriteLine("no such option!");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ homework2/App2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, e;
+            Console.WriteLine("Please enter first number ..");
+
+            string x = Console.ReadLine();
+            a = Convert.ToInt32(x);
+            Console.WriteLine(a);
+
+            Console.WriteLine("Please enter second number ..");
+            string y = Console.ReadLine();
+            b = Convert.ToInt32(y);
+            Console.WriteLine(b);
+
+            Console.WriteLine("Please enter third number ..");
+            string z = Console.ReadLine();
+            c = Convert.ToInt32(z);
+            Console.WriteLine(c);
+
+            Console.WriteLine("Please enter last number ..");
+            string o = Console.ReadLine();
+            e = Convert.ToInt32(o);
+            Console.WriteLine(e);
+
+            int result = (a + b + c + e) / 4;
+            Console.WriteLine("The average of " + a + ", " + b + ", " + c + " and " + e + " is: " + result);
+            Console.ReadLine();
+        }
+    }
+}
